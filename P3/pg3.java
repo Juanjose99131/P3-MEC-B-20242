8daba6f8c15e066f7c69e10a9fa112f01dd0654b
Juanjose99131 <est.juan.jpena@unimilitar.edu.co>
2024-08-27 15:43:22 -0500

    Create pg3.java

---
+++ P3/pg3.java
@@ -0,0 +1,13 @@
+package com.mycompany.traingulo;
+
+//***crear dos metos puro que permita calcular area  y perimetro asumuniendo que son trianguos rectangulos/
+  
+
+public class Traingulo {
+    
+    
+        int largo, ancho;
+        int perimietro;
+        int perimitro=0;        
+
+}
