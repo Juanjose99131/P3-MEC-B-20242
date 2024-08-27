50c14a4f5d922e3b52f3fc2a9bfd0915f91be558
Juanjose99131 <est.juan.jpena@unimilitar.edu.co>
2024-08-27 15:33:08 -0500

    Create pg2.java

---
+++ P2/pg2.java
@@ -0,0 +1,17 @@
+/*
+ * apartir de la clase anteriror crear sobrecarga al constructor y crear dos objetos para cda una de las formas del /L/Licic/enses/license-default.txt to change this license
+ */
+
+public class Traingulo {
+    
+    public static void main(String[] args) {
+        System.out.println("parametro vacio!");
+        
+    }
+    public class ancho {
+    public static void main(String[] args) {
+        String ancho;
+                 System.out.println("parametro!" + ancho  );
+    }
+}
+}
